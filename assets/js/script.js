5e597c11922f20380610a3ee0c8a443e45035f5e
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-07-28 00:27:58 -0500

    terminando clase 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,17 @@ const createTask = (e) => {
     e.preventDefault();
     const input =document.querySelector('[data-form-input]');
     const value = input.value;
-    console.log(value);
+    input.value='';
+    const task=document.querySelector('[data-task-list]');
+    const content= ` 
+    <li class="card">
+        <div>
+        <i class="far fa-check-square icon"></i>
+            <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i> 
+    </li>`;
+    task.innerHTML+=content;
 
 }
 
